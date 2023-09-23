e4cdbecf4f04b207afe73ef06c28013b33fc6e0b
muris11 <126364891+muris11@users.noreply.github.com>
2023-09-23 15:29:41 +0700

    upload fie web kelas
    upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const txtElement = ['Mahasiswa', 'Politeknik', 'Negeri', 'Idramayu'];
+let count = 0;
+let txtIndex = 0;
+let currenTxt = '';
+let words = '';
+
+(function ngetik(){
+
+    if(count == txtElement.length){
+        count = 0;
+    }
+    currenTxt = txtElement[count];
+
+    words = currenTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if(words.length == currenTxt.length) {
+        count++;
+        txtIndex = 0;
+
+    }
+    setTimeout (ngetik, 150);
+
+
+})();
